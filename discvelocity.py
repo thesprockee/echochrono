1ee4af5fb634e1e63b1daeeab6a442d1b34f6b5c
Andrew Bates <andrew@sprock.io>
2020-10-04 17:59:57 -0700

    Add TTS options

--- discvelocity.py
+++ discvelocity.py
@@ -47,18 +47,41 @@ def get_session_frame(url):
         click.echo('Invalid json.')
         return {}
 
-def _get_possession(frame):
+def _get_player_with_possession(frame):
     """ Return the name of the player with posession of the disc """
     for t in frame['teams']:
-        if 'players' in t:
-            for p in t['players']:
-                if p.get('possession'):
-                    return p['name']
-    return 'None'
+        for p in t.get('players',[]):
+            if p.get('possession'):
+                return p['name']
+    return None
+
+
+class Chronograph(object):
+
+    def __init__(self, recordpath=None,):
+        self.speeds = []
+        self.players = {}
+        self.armed = True
+
+    def loop(self, object):
+       Screen.wrapper(self.display_loop)
+
+    def display_loop(self, screen):
+        while True:
+            for n, playerspeed in enumerate(self.players):
+
+                screen.print_at(
+                    FigletText("14.3", font='doh'),
+                    n, 0,
+                )
+
+                screen.refresh()
+
+
 
 
 @click.argument('questhost', metavar='<QUEST IP>')
-@click.option('-r', '--rate', help='Disc velocity update/refresh rate in Hz',
+@click.option('-r', '--refresh-rate', 'refreshrate', help='Disc velocity update/refresh rate in Hz',
               type=float, metavar='HERTZ',  default=5)
 @click.option('-m', '--minimum-speed', 'minspeed',
               help='Minimum disc speed to read out', type=float,
@@ -76,11 +99,21 @@ def _get_possession(frame):
               help='figlet font to use for banner')
 @click.option('--debug', 'debug', is_flag=True, default=False,
               help='Print lots of extra debug messages')
+@click.option('--tts-options', 'ttsoptions', default='rate=125',
+              help='TTS engine options')
 @click.command(context_settings=dict(show_default=True,
                                      help_option_names=['-h', '--help']))
-def main(questhost, rate, minspeed, notts, nobanner, tolerance, font,
-         recordpath, debug, engine=pyttsx3.init()):
+def main(questhost, refreshrate, minspeed, notts, nobanner, tolerance, font,
+         recordpath, debug, ttsoptions, _engine=pyttsx3.init()):
     """ Chronograph for Echo Arena on the Oculus Quest """
+
+    if not notts:
+        for prop, val in [s.strip().split('=') for s in ttsoptions.split(',')]:
+            prop = prop.strip()
+            val = val.strip()
+            val = float(val) if val.isdecimal() else val
+            _engine.setProperty(prop, val)
+
 
     speeds = []
     armed = True
@@ -144,8 +177,8 @@ def main(questhost, rate, minspeed, notts, nobanner, tolerance, font,
                 click.echo(Figlet(font=font).renderText(str(bannertext)))
 
             if not notts:
-                engine.say('{:.1f}'.format(speed))
-                engine.runAndWait()
+                _engine.say('{:.1f}'.format(speed))
+                _engine.runAndWait()
                 armed = False
 
         #click.echo(print_big('{:.1f}'.format(speed)), color='green')
